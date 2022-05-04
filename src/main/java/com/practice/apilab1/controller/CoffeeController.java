9a332994e3c84dab7a326313e579ce31c3dafdfc
Pratibha <psharma@atlassian.com>
2022-05-04 16:39:55 +0100

    red test case for coffee controller

---
+++ src/main/java/com/practice/apilab1/controller/CoffeeController.java
@@ -0,0 +1,4 @@
+package com.practice.apilab1.controller;
+
+public class CoffeeController {
+}
---
+++ src/test/java/com/practice/apilab1/controller/CoffeeControllerTest.java
@@ -0,0 +1,27 @@
+package com.practice.apilab1.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class CoffeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvcController;
+
+    @Test
+    public void testGetCoffeeLover() throws Exception {
+        String expectedContent = "I like Coffee!";
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/coffeelover"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(expectedContent));
+    }
+}
