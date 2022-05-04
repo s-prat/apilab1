8ed15b181d1b7a6441d470bfb131cc407c7935e2
Pratibha <psharma@atlassian.com>
2022-05-04 16:43:12 +0100

    added method in CoffeeController which resulted in green test case

--- src/main/java/com/practice/apilab1/controller/CoffeeController.java
+++ src/main/java/com/practice/apilab1/controller/CoffeeController.java
@@ -1,4 +1,14 @@
 package com.practice.apilab1.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CoffeeController {
+
+    @RequestMapping(value="/coffeelover", method= RequestMethod.GET)
+    public String getCoffeeLover() {
+        return "I like Coffee!";
+    }
 }
